e21d1056b5bf8226b53c2635fe6c50f8ddb4f33b
lijiahong <godancekimo@gmail.com>
2018-07-08 10:25:13 +0800

    2018070803

--- src/item8.js
+++ src/item8.js
@@ -10,6 +10,8 @@ var Item8Layer = cc.Layer.extend({
         this.sprite = new cc.Sprite(res.s1_0025_png);
         this.sprite.attr({x:cc.winSize.width/2, y:cc.winSize.height/2});
         this.addChild(this.sprite);
+        // var a1 = cc.moveTo(3,100,100);
+        this.sprite.runAction(a1);
 
         return true;
     }
